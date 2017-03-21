21dcdfd7e008bc2a79614f8a193b6fc37434f762
esensar <es.ensar@gmail.com>
2017-03-21 22:14:59 +0100

    Init project

---
+++ users/src/main/java/ba/steleks/SteleksServiceApplication.java
@@ -0,0 +1,12 @@
+package ba.steleks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteleksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteleksServiceApplication.class, args);
+	}
+}
