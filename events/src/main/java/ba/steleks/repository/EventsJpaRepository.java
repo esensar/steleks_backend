91a3961a0818e3c21cf0c80ca6252e76aabd6029
dizda13 <dino_dizda@live.com>
2017-03-23 03:50:45 +0100

    Events and JPA Events Reposytory

--- events/src/main/java/ba/steleks/repository/EventsJpaRepository.java
+++ events/src/main/java/ba/steleks/repository/EventsJpaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
-//@RepositoryRestResource(collectionResourceRel = "events", path = "events")
+@RepositoryRestResource(collectionResourceRel = "events", path = "events")
 public interface EventsJpaRepository extends PagingAndSortingRepository<Event, Long> {
 
     Event save(Event event);
