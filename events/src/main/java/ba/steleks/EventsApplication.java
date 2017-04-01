be0e2347e5f408d020f5adc4de2c538905f2e929
dizda13 <dino_dizda@live.com>
2017-04-01 14:00:40 +0200

    some service and encryption

--- events/src/main/java/ba/steleks/EventsApplication.java
+++ events/src/main/java/ba/steleks/EventsApplication.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -22,8 +24,24 @@ class MessageRestController {
 	@Value("${message:Hello default}")
 	private String message;
 
+	@Value("${user.password}")
+	private String password;
+
 	@RequestMapping("/message")
 	String getMessage() {
 		return this.message;
 	}
+
+	//@RequestMapping(path ="/temp", value = "/{id}", method = RequestMethod.GET)
+	//String getTemp(@PathVariable("id") Long id){
+	//	return "temp " + Long.toString(id);
+	//}
+
+	@RequestMapping(value = "/whoami/{user}", method = RequestMethod.GET)
+	public String whoami(@PathVariable("username") String username) {
+		return String.format("Hello! You're %s and you'll become a(n) root, " +
+		"but only if your password is '%s'!\n",
+				username, password);
+	}
 }
+
