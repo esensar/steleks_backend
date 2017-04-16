f29c553ec9d890a26c30cf99a143531dc2323ba6
dizda13 <dino_dizda@live.com>
2017-04-16 20:57:13 +0200

    decryption

--- users/src/main/java/ba/steleks/SteleksServiceApplication.java
+++ users/src/main/java/ba/steleks/SteleksServiceApplication.java
@@ -2,14 +2,17 @@ package ba.steleks;
 
 import ba.steleks.storage.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,8 +30,25 @@ public class SteleksServiceApplication {
 @RestController
 class ServiceInstanceRestController {
 
-	@Autowired
+
 	private DiscoveryClient discoveryClient;
+
+	@Autowired
+	public ServiceInstanceRestController(DiscoveryClient discoveryClient) {
+		this.discoveryClient=discoveryClient;
+	}
+
+	@Value("${user.password}")
+	private String password;
+
+	@RequestMapping(
+			value = "/whoami/{username}",
+			method = RequestMethod.GET,
+			produces = MediaType.TEXT_PLAIN_VALUE)
+	public String whoami(@PathVariable("username") String username) {
+		return String.format("Hello! You're %s and you'll become Developer, " +
+				"but only if your password is '%s'!\n", username, password);
+	}
 
 	@RequestMapping("/service-instances/{applicationName}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(
