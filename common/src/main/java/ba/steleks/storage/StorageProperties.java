c2f56bc23bd9355d4111a1cee868487bc370846b
esensar <es.ensar@gmail.com>
2018-01-17 22:21:25 +0100

    Make storage directory configurable

--- common/src/main/java/ba/steleks/storage/StorageProperties.java
+++ common/src/main/java/ba/steleks/storage/StorageProperties.java
@@ -1,5 +1,6 @@
 package ba.steleks.storage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("storage")
@@ -8,7 +9,8 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "/Users/admin";
+    @Value("{default.storage.directory}")
+    private String location;
 
     public String getLocation() {
         return location;
