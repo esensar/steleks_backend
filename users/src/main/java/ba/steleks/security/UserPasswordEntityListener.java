7df33fd942807d27f24791a3f22b6082e5ab1633
esensar <es.ensar@gmail.com>
2018-01-17 22:12:22 +0100

    Make all new users users by default

--- users/src/main/java/ba/steleks/security/UserPasswordEntityListener.java
+++ users/src/main/java/ba/steleks/security/UserPasswordEntityListener.java
@@ -2,14 +2,17 @@ package ba.steleks.security;
 
 import ba.steleks.AutowireHelper;
 import ba.steleks.model.User;
+import ba.steleks.repository.UserRolesJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import javax.persistence.PostPersist;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import java.util.HashSet;
 
 /**
  * Created by ensar on 30/05/17.
@@ -21,6 +24,9 @@ public class UserPasswordEntityListener {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserRolesJpaRepository userRolesJpaRepository;
+
     @PrePersist
     @PreUpdate
     public void onUserUpdate(User user) {
@@ -28,6 +34,15 @@ public class UserPasswordEntityListener {
         if(user.getPassword() != null) {
             user.setPasswordHash(passwordEncoder.encode(user.getPassword()));
         }
+    }
+
+    @PostPersist
+    public void postUserCreated(User user) {
+        AutowireHelper.autowire(this, userRolesJpaRepository);
+        if (user.getUserRoles() == null) {
+            user.setUserRoles(new HashSet<>());
+        }
+        user.getUserRoles().add(userRolesJpaRepository.findOne(1L));
     }
 
 }
--- users/src/main/java/ba/steleks/security/UserRoleFactory.java
+++ users/src/main/java/ba/steleks/security/UserRoleFactory.java
@@ -39,6 +39,7 @@ public class UserRoleFactory {
                 .stream()
                 // get role name
                 .map(UserRole::getRoleName)
+                .map(role -> "ROLE_" + role)
                 // create authority
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
