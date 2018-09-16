93347be39049c39573375070c56c986a1f6bd378
Dino Dizdarevic <dizda13@Dinos-MacBook-Pro-2.local>
2018-09-16 23:23:54 +0200

    user patch

--- users/src/main/java/ba/steleks/security/AuthenticationFilter.java
+++ users/src/main/java/ba/steleks/security/AuthenticationFilter.java
@@ -35,7 +35,6 @@ public class AuthenticationFilter extends GenericFilterBean {
                 tokenStore,
                 usersJpaRepository
         );
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
